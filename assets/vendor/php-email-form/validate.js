9a307b5e9ff5406262bd6dd7149baea1a88b55c5
Blanco Soft <Info@blancosoft.net>
2024-12-22 09:46:02 +0300

    go live

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -6,7 +6,7 @@
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  let forms = document.querySelectorAll('.email-form');
 
   forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
@@ -31,7 +31,7 @@
         if(typeof grecaptcha !== "undefined" ) {
           grecaptcha.ready(function() {
             try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+              grecaptcha.execute(recaptcha, {action: 'email_form_submit'})
               .then(token => {
                 formData.set('recaptcha-response', token);
                 php_email_form_submit(thisForm, action, formData);
